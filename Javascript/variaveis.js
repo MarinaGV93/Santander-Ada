6f5597c7b43ca87096347bc6951c3425ab755086
Projeto <marinasgv@hotmail.com>
2023-11-13 19:36:04 -0300

    Variáveis

---
+++ Javascript/variaveis.js
@@ -0,0 +1,33 @@
+// Criar variavel
+let idade = 30
+
+// Não pode ser alterada
+const constante = 100
+
+// Tipos de variaveis
+let numero = 30 //Number
+let altura = 1.77 //Nummber
+let nome = "Marina" //String
+let estudando = true //Booleano
+
+// Alterar conteudo variavel
+idade = 25
+
+// Imprimir variavel. Usa , para imprimir outra coisa
+// Typeof = Tipo da variavel
+console.log(idade, typeof idade);
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+// Sem atribuir algo
+// let outra;
+    // OU
+let outra = undefined
+
+// Nulo
+let nula = null
+
+console.log(outra);
+console.log(nula);
