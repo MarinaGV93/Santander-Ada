c3497ee4a583a1585ddfc9f78bc89605f27ca04a
Projeto <marinasgv@hotmail.com>
2023-11-13 22:10:03 -0300

    Operedores Aritméticos

---
+++ Javascript/operadores_aritmeticos.js
@@ -0,0 +1,16 @@
+let n1 = 20
+let n2 = 2
+
+console.log('Soma = ', n1 + n2);
+console.log('Subtração = ', n1 - n2);
+console.log('Multiplicação = ', n1 * n2);
+console.log('Divisão = ', n1 / n2);
+console.log('Exponenciação = ', n1 ** n2);
+console.log('Resto da divisão = ', n1 % n2);
+
+// Precedência de operadores. Ordem 
+console.log(3 + 7 * 3);
+
+// Biblioteca Math
+// Calcular raiz quadrada
+console.log(Math.sqrt(16));
