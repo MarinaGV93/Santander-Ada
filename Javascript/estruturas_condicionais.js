d1124da9f3a852f88a0cd107096d18aa944cb0e8
Projeto <marinasgv@hotmail.com>
2023-11-15 19:53:23 -0300

    Estrutuas condicionais

---
+++ Javascript/estruturas_condicionais.js
@@ -0,0 +1,28 @@
+const idade = 30
+
+//Se...Entao
+if (idade >= 18){
+
+      //Se verdadeiro
+      console.log('Maior de idade')
+} else{
+
+      //Se falso
+      console.log('Menor de idade')
+}
+
+/* Se média >= 7 = aprovado
+Se média = 7 = recuperaçãp
+Se média < 7 = reprovado
+*/
+
+let media = 10
+
+if (media >= 7){
+      console.log('Aprovado(a)')
+  //Meio termo
+} else if (media >= 5){
+      console.log('Recuperação')
+} else{
+      console.log('Reprovado(a)')
+}
